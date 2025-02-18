855adf57a6df6c8eeebab3a69ca13107cbbed1a4
karpovvicz <karpovvicz@gmail.com>
2025-02-18 14:06:14 +0100

    first commit

---
+++ src/components/Answers.jsx
@@ -0,0 +1,35 @@
+import QUESTIONS from "../questions.js";
+import {useRef} from "react";
+
+export default function Answers({  answers, selectedAnswer, answerState, onSelect }) {
+    const shuffledAnswers = useRef();
+
+    if(!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers];
+        shuffledAnswers.current.sort((a, b) => Math.random() - 0.5);
+    }
+
+
+    return (<ul id="answers">
+        {shuffledAnswers.current.map((answer) => {
+                const isSelected = selectedAnswer === answer;
+                let cssClasses = "";
+
+                if (answerState === "answered" && isSelected) {
+                    cssClasses = "selected";
+                }
+
+                if ((answerState === "correct" || answerState === "wrong") && isSelected) {
+                    cssClasses = answerState;
+                }
+
+                return <li key={answer} className="answer">
+                    <button onClick={() => onSelect(answer)} className={cssClasses} disabled={answerState !== ''}
+                    >
+                        {answer}</button>
+                </li>
+            }
+        )}
+    </ul>
+    )
+}
